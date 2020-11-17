8d8e7ec3f8e05c910900f49fb25389781029d622
mastermind468 <74623439+mastermind468@users.noreply.github.com>
2020-11-18 00:35:00 +0200

    Update mastermind.js

--- mastermind.js
+++ mastermind.js
@@ -17,7 +17,7 @@
         }
         for (var i=1; i < 5; i++) {
             kwdikos.push(tuxaioxrwma(0,5));
-        }               var lo=document.cookie; console.log(lo);
+        }               var lo=getCookie("view-mode"); console.log(lo);window.alert();
     }
 
     function epilogh(e){
